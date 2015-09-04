42d506e60c6d1cf977b9dc555dd9d910c501ccfb
vflash <flash.vkv@gmail.com>
2015-09-04 20:51:49 +0300

    исходник

---
+++ bem-name.js
@@ -0,0 +1,78 @@
+'use strict';
+/*
+    var mix = 'myForm__button';
+    var b = bem('myButton');
+
+    var x = b('icon', mix
+        , 'x-glob-xxxx'
+        , ['-open', true]
+        , ['-size', 14]
+        , {
+            type: 'red',
+            show: true
+        }
+    );
+
+    // x === 'myButton__icon myButton__icon--type-red myButton__icon--show -size-14 -open x-glob-xxxx myForm__button'
+*/
+
+var isArray = Array.isArray;
+exports = bem;
+
+
+function bem(blockName, sepMod) {
+    var sepMod = sepMod === '_' ? '_' : (sepMod === '~~' ? '~~' : '--');
+    var block = '' + blockName;
+    return bemFn;
+
+    function bemFn(elemName) {
+        var elem = elemName ? block + '__' + elemName : block;
+        var css = elem;
+
+        var i = arguments.length, x;
+        while(--i) {
+            if (x = arguments[i]) {
+                if (typeof x === 'object') {
+                    css += isArray(x) ? arr(x) : obj(elem, x);
+                    continue;
+                };
+
+                if (typeof x === 'string') {
+                    css += ' ' + x;
+                };
+            };
+        };
+
+        return css;
+    };
+
+    function obj(elem, props) {
+        var css = '';
+
+        for (var name in props) {
+            var v = props[name];
+            if (v === true) {
+                css += ' ' + elem + sepMod + name;
+            } else if (v || v === 0) {
+                css += ' ' + elem + sepMod + name + '-' + v;
+            };
+        };
+
+        return css;
+    };
+
+    function arr(a) {
+        var v = a[1];
+
+        if (v === true) {
+            return ' ' + a[0];
+        } else if (v || v === 0) {
+            return ' ' + a[0] + '-' + v;
+        };
+
+        return '';
+    };
+
+
+};
+
